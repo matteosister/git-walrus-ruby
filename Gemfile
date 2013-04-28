06b7575118e9a8536ac926a38db4a2cf0af5f847
Matteo <matteog@gmail.com>
2013-04-28 23:03:30 +0200

    padrino start

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+# Server requirements
+# gem 'thin' or mongrel
+
+# Project requirements
+gem 'rake'
+gem 'rack-flash'
+
+# Component requirements
+gem 'haml'
+
+# Test requirements
+
+
+# Padrino
+gem 'padrino', '0.10.2'
+# Padrino EDGE
+# gem 'padrino', :git => 'git://github.com/padrino/padrino-framework.git'
+#
+# Individual GEMS
+# gem 'padrino-core', '0.10.2'
+# gem 'padrino-admin', '0.10.2'
+# gem 'padrino-cache', '0.10.2'
+# gem 'padrino-gen', '0.10.2'
+# gem 'padrino-helpers', '0.10.2'
+# gem 'padrino-mailer', '0.10.2'
---
+++ files/controllers/root.rb
@@ -0,0 +1,29 @@
+Files.controllers :root do
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  get :si, :map => '/si' do
+    'si'
+  end
+
+  get :index, :map => '/' do
+    render 'root/index', :layout => :'layouts/files.html'
+  end
+  
+end
