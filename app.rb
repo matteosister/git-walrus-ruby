40826542ccd69be263d23d294a184a0f568e74c1
Matteo <matteog@gmail.com>
2013-05-10 00:11:41 +0200

    tree

--- app.rb
+++ app.rb
@@ -2,12 +2,34 @@ require 'rubygems'
 require 'sinatra'
 require 'grit'
 require 'haml'
+require 'sass'
 
 set :haml, {:format => :html5}
 set :scss, {:views => 'scss'}
 
+#@repo = Grit::Repo.new('/home/matteo/libraries/GitElephant')
+#@path = 'src'
+#@tree = @repo.tree('develop')/@path
+#@tree.trees.each do |t|
+#  puts t.name
+#end
+
 before do
   @repo = Grit::Repo.new('/home/matteo/libraries/GitElephant')
+end
+
+get '/tree/:ref/*' do
+  @ref = params[:ref]
+  @path = params[:splat].first.to_s
+  if 0 === @path.count('/')
+    @back = '/'
+  else
+    newPath = @path.split('/')[0..-2].join('/')
+    @back = "/tree/#{ @ref }/#{ newPath }"
+  end
+  puts @back
+  @tree = @repo.tree/@path
+  haml :tree
 end
 
 get '/all.css' do
@@ -16,5 +38,7 @@ get '/all.css' do
 end
 
 get '/' do
-  haml :homepage, :repo => @repo
+  @ref = 'master'
+  @tree = @repo.tree(@ref)
+  haml :homepage, :layout => :layout
 end
